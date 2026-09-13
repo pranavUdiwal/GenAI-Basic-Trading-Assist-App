6ef7de5f85a7f1db1fd5499c579699306a11140e
pranavUdiwal <pranavudiwal@gmail.com>
2026-09-14 01:55:22 +0530

    Remove leftover auth and unused frontend files

--- frontend/src/api/experimentApi.js
+++
@@ -1,6 +0,0 @@
-import axiosClient from './axiosClient';
-
-export const postExperiment = async (conversation) => {
-  const { data } = await axiosClient.post('/conversation/experiment', { conversation });
-  return data;
-};
